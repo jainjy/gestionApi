7c347865bd94c37b6647d6f25a8d39896481b24b
jainjy <ramamonjisoaandrianina@gmail.com>
2025-10-17 17:35:06 +0300

    ok

--- controllers/metiersController.js
+++ controllers/metiersController.js
@@ -1,5 +1,4 @@
-// server/controllers/metiersController.js
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 class MetiersController {
