1d2aca74013a3906f65e8c0cb6558dcddf3ccbe7
manonga123 <mandakely2004@gmail.com>
2025-11-26 12:04:25 +0300

    bvlk

---
+++ prisma/migrations/20251126081505_add_reservation_flight_user_relation/migration.sql
@@ -0,0 +1,25 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `productType` on the `Product` table. All the data in the column will be lost.
+
+*/
+-- DropIndex
+DROP INDEX "public"."Product_productType_idx";
+
+-- AlterTable
+ALTER TABLE "Product" DROP COLUMN "productType";
+
+-- CreateTable
+CREATE TABLE "ReservationFlight" (
+    "id" TEXT NOT NULL,
+    "flightId" TEXT NOT NULL,
+    "idUser" TEXT NOT NULL,
+    "idPrestataire" TEXT NOT NULL,
+    "nbrPersonne" INTEGER NOT NULL,
+    "place" TEXT NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'pending',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "ReservationFlight_pkey" PRIMARY KEY ("id")
+);
