5d2490c9ac2f4dcf3ad666d844117e108be67ca3
jainjy <ramamonjisoaandrianina@gmail.com>
2026-01-20 15:11:26 +0300

    seed new admin

--- prisma/seeders/seedNewAdmin.js
+++ prisma/seeders/seedNewAdmin.js
@@ -1,4 +1,44 @@
-// prisma/seedNewAdmin.js
+// // prisma/seedNewAdmin.js
+// const { PrismaClient } = require("@prisma/client");
+// const bcrypt = require("bcryptjs");
+
+// const prisma = new PrismaClient();
+
+// async function main() {
+//   console.log("🌱 Début du seeding du nouveau compte admin...");
+
+//   // Hasher le mot de passe
+//   const saltRounds = 12;
+//   const hashedAdminPassword = await bcrypt.hash("admin@oli26", saltRounds);
+
+//   // Créer le compte admin
+//   const newAdmin = await prisma.user.create({
+//     data: {
+//       email: "admin@oliplus.re",
+//       passwordHash: hashedAdminPassword,
+//       firstName: "Admin",
+//       lastName: "OliPlus",
+//       phone: "+261 34 12 345 80",
+//       role: "admin",
+//       userType: "ADMIN",
+//       status: "active",
+//     },
+//   });
+
+//   console.log("✅ Nouveau compte admin créé avec succès !");
+//   console.log("📋 Compte : admin@oliplus.re / admin123");
+// }
+
+// main()
+//   .catch((e) => {
+//     console.error("❌ Erreur lors du seeding:", e);
+//     process.exit(1);
+//   })
+//   .finally(async () => {
+//     await prisma.$disconnect();
+//   });
+
+//prisma/seedNewAdmin.js
 const { PrismaClient } = require("@prisma/client");
 const bcrypt = require("bcryptjs");
 
@@ -9,24 +49,26 @@ async function main() {
 
   // Hasher le mot de passe
   const saltRounds = 12;
-  const hashedAdminPassword = await bcrypt.hash("admin@oli26", saltRounds);
+  const hashedAdminPassword = await bcrypt.hash("direction@oli26", saltRounds);
 
   // Créer le compte admin
   const newAdmin = await prisma.user.create({
     data: {
-      email: "admin@oliplus.re",
+      email: "direction@oliplus.re",
       passwordHash: hashedAdminPassword,
       firstName: "Admin",
       lastName: "OliPlus",
-      phone: "+261 34 12 345 80",
+      phone: "+33 6 92 66 77 55",
       role: "admin",
       userType: "ADMIN",
       status: "active",
+      avatar:
+        "https://wvrxayklhpbquxsluzve.supabase.co/storage/v1/object/public/blog-images/blog-images/1768826569336-mey1yjm3q4.png",
     },
   });
 
   console.log("✅ Nouveau compte admin créé avec succès !");
-  console.log("📋 Compte : admin@oliplus.re / admin123");
+  console.log("📋 Compte : direction@oliplus.re / direction123");
 }
 
 main()
@@ -37,3 +79,4 @@ main()
   .finally(async () => {
     await prisma.$disconnect();
   });
+
